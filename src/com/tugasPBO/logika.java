d37fe1089315f952fa94381e27d9ac23121a8b5a
unknown <argya.programmer@gmail.com>
2020-10-17 14:37:44 +0700

    Initial Commit

---
+++ src/com/tugasPBO/logika.java
@@ -0,0 +1,15 @@
+//latihan 10 PBO
+// Nama       : Argya Aulia Fauzandika
+// NIM        : 10119035
+// Kelas      : IF-1
+// Deskripsi  : menampilkan logika true atau false
+package com.tugasPBO;
+
+public class logika {
+    public static void main(String[] args){
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
